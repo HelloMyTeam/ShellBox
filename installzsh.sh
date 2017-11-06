d1e987d5d837366bd351d697a1c4a95ca5e7be75
hello--world <hellohfy@gmail.com>
2017-11-06 14:55:39 +0800

    Update installzsh.sh

--- installzsh.sh
+++ installzsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+yum update -y &&
 echo 'y' | yum install zsh -y && 
 echo 'y' | yum install git -y && 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
