76115683b10b22613f67069b8d05d4b9b6da66cf
hello--world <hellohfy@gmail.com>
2017-11-06 16:40:29 +0800

    Update installmysql.sh

--- installmysql.sh
+++ installmysql.sh
@@ -14,12 +14,10 @@ filemd5="`md5sum ${name}`"
 if [[ ${filemdt} -eq ${MD5} ]];then
 echo "mysql file is ok"
 sudo rpm -ivh ${name} 
-echo "y" | sudo yum -y update &&
-echo "y" | sudo yum -y install mysql-server &&
-sudo systemctl start mysqld &&
-
+echo "y" | sudo yum -y update && \
+echo "y" | sudo yum -y install mysql-server && \
+sudo systemctl start mysqld && \
+sudo grep 'temporary password' /var/log/mysqld.log
 echo "if use python, you need execute: yum install python-devel mysql-community-devel -y"
 rm ${name}
-sudo grep 'temporary password' /var/log/mysqld.log
-
 fi
