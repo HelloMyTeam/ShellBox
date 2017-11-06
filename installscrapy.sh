8efbc4e26a1847c175bcd086456e27eba75a44e9
hello--world <hellohfy@gmail.com>
2017-11-06 16:37:56 +0800

    Update installscrapy.sh

--- installscrapy.sh
+++ installscrapy.sh
@@ -3,6 +3,5 @@
 sudo yum -y install gcc gcc-c++ kernel-devel
 sudo yum -y install python-devel libxslt-devel libffi-devel openssl-devel
 pip install cryptography
-sudo yum install gcc
 sudo pip install --upgrade pip
 sudo pip install scrapy
