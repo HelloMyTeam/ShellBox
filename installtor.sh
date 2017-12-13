533bbfbc22ac25a0d0ffe5927ded91c4d4bc0cf1
hello--world <hellohfy@gmail.com>
2017-12-13 16:25:19 +0800

    Update installtor.sh

--- installtor.sh
+++ installtor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+yum install epel-release
 echo "y" | yum install tor -y
 echo "y" | yum install  privoxy -y
 echo "/etc/tor/torrc ControlPort 9051 /etc/privoxy/config listen-address  0.0.0.0:31111 forward-socks5 / 127.0.0.1:9050"
