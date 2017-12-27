48498a8ed5d4ca9a5215fdde5c699c1f9bd93530
hello--world <hellohfy@gmail.com>
2017-12-27 17:57:59 +0800

    Update installzsh.sh

--- installzsh.sh
+++ installzsh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yum update -y &&
-echo 'y' | yum install zsh -y && 
-echo 'y' | yum install git -y && 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+yum update -y && \
+yum install zsh -y && \
+yum install git -y && \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc && source ~/.zshrc
