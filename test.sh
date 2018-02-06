4ef59261e485b0c5d70ce62966bf13a05c107e0c
hello--world <hellohfy@gmail.com>
2018-02-06 14:45:07 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+sysOS=`uname -s`
+
+if [ $sysOS == "Darwin" ];then
+        which brew
+        if [[ $? != 0 ]];then
+            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+    elif [ $sysOS == "Linux" ];then
+        curl -L https://raw.githubusercontent.com/hello--world/Shell/master/installzsh.sh | bash
+        sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc && source ~/.zshrc
+    else
+	    echo "Other OS: $sysOS"
+    fi
