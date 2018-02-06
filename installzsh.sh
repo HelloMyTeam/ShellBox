4f06f6e4de7088040bce8b51555697386cc35f83
hello--world <hellohfy@gmail.com>
2018-02-06 14:43:12 +0800

    test

--- installzsh.sh
+++ installzsh.sh
@@ -3,4 +3,4 @@ yum update -y && \
 yum install zsh -y && \
 yum install git -y && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc && source ~/.zshrc
+echo "sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc && source ~/.zshrc"
