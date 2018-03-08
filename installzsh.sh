362e5943d6c5a73e93a8a5f985117f800092c329
hfy-iMac <hellohfy@gmail.com>
2018-03-08 16:20:15 +0800

    zsh

--- installzsh.sh
+++ installzsh.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-yum update -y && \
-yum install zsh -y && \
-yum install git -y && \
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc && source ~/.zshrc"
+
+sysOS=`uname -s`
+
+if [ $sysOS == "Darwin" ];then
+    which brew
+    if [[ $? != 0 ]];then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+elif [ $sysOS == "Linux" ];then
+    yum update -y && \
+    yum install zsh -y && \
+    yum install git -y && \
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo "sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc && source ~/.zshrc"
+	sed -i 's/robbyrussell/af-magic/g' ~/.zshrc && source ~/.zshrc   
+else
+	echo "Other OS: $sysOS"
+fi
