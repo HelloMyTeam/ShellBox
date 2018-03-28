d023dceae7e0707b1a65f63cc7fcd4b8361702c7
hfy-iMac <test@gmail.com>
2018-03-28 10:48:31 +0800

    build ffmpeg

--- centos7_build_ffmpeg.sh
+++ centos7_build_ffmpeg.sh
@@ -17,7 +17,7 @@ yasm () {
   curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
   tar xzvf yasm-1.3.0.tar.gz
   cd yasm-1.3.0
-  ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+  ./configure --prefix="$HERE/ffmpeg_build" --bindir="$HERE/bin"
   make && make install
 }
 
@@ -26,7 +26,7 @@ x264 () {
   cd ${HERE}/ffmpeg_sources
   git clone --depth 1 http://git.videolan.org/git/x264
   cd x264
-  PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+  PKG_CONFIG_PATH="$HERE/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HERE/ffmpeg_build" --bindir="$HERE/bin" --enable-static
   make && make install
 }
 
@@ -35,7 +35,7 @@ x265 () {
   cd ${HERE}/ffmpeg_sources
   hg clone https://bitbucket.org/multicoreware/x265
   cd ${HERE}/ffmpeg_sources/x265/build/linux
-  cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+  cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HERE/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
   make && make install
 }
 
@@ -45,27 +45,27 @@ fdk-aac () {
   git clone --depth 1 https://github.com/mstorsjo/fdk-aac
   cd fdk-aac
   autoreconf -fiv
-  ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+  ./configure --prefix="$HERE/ffmpeg_build" --disable-shared
   make && make install
 }
 
-六、lame () {
+lame () {
   # 六、lame   #MP3 音频编码器.
   cd ${HERE}/ffmpeg_sources
   curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
   tar xzvf lame-3.99.5.tar.gz
   cd lame-3.99.5
-  ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+  ./configure --prefix="$HERE/ffmpeg_build" --bindir="$HERE/bin" --disable-shared --enable-nasm
   make && make install
 }
 
-七、opus () {
+opus () {
   # 七、opus  #Opus 音频编解码器.
   cd ${HERE}/ffmpeg_sources
   curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz
   tar xzvf opus-1.1.5.tar.gz
   cd opus-1.1.5
-  ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+  ./configure --prefix="$HERE/ffmpeg_build" --disable-shared
   make && make install
 }
 
@@ -75,7 +75,7 @@ ogg () {
   wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
   tar xzvf libogg-1.3.2.tar.gz
   cd libogg-1.3.2
-  ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+  ./configure --prefix="$HERE/ffmpeg_build" --disable-shared
   make && make install
 }
 
@@ -85,7 +85,7 @@ vorbis () {
   wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
   tar xf libvorbis-1.3.4.tar.gz
   cd libvorbis-1.3.4
-  ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+  ./configure --prefix="$HERE/ffmpeg_build" --with-ogg="$HERE/ffmpeg_build" --disable-shared
   make && make install
 }
 
@@ -94,30 +94,33 @@ vpx () {
   cd ${HERE}/ffmpeg_sources
   git clone https://github.com/webmproject/libvpx.git
   cd libvpx
-  ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
-  PATH="$HOME/bin:$PATH" make
+  ./configure --prefix="$HERE/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
+  PATH="$HERE/bin:$PATH" make
   make install
 }
 
+ffmpeg_build () {
+  # 十一、ffmpeg
+  ffmpegversion='3.4'
+  ffmpegfile="ffmpeg-${ffmpegversion}.tar.xz"
+  cd ${HERE}/ffmpeg_sources
 
-# 十一、ffmpeg
-ffmpegversion='3.4'
-ffmpegfile="ffmpeg-${ffmpegversion}.tar.xz"
-cd ${HERE}/ffmpeg_sources
-ls
-if [[ -f ${ffmpegfile} ]];then
-  # m=$(`md5sum $ffmpegfile | cut -d " " -f 1`)
-  # if [[ "$m" -ne "c64ba7247bb91e516f6a5789348fd5b5" ]];then
-  wget http://ffmpeg.org/releases/${ffmpegfile} 
+  if [[ -f ${ffmpegfile} ]];then
+    # m=$(`md5sum $ffmpegfile | cut -d " " -f 1`)
+    # if [[ "$m" -ne "c64ba7247bb91e516f6a5789348fd5b5" ]];then
+    wget http://ffmpeg.org/releases/${ffmpegfile} 
+    echo "download ${ffmpegfile}"
+    # fi
+  else
+  wget http://ffmpeg.org/releases/${ffmpegfile}
   echo "download ${ffmpegfile}"
-  # fi
-else
-wget http://ffmpeg.org/releases/${ffmpegfile}
-echo "download ${ffmpegfile}"
-fi
+  fi
+  tar xvf ${ffmpegfile}
+  cd ffmpeg-${ffmpegversion}
+  PKG_CONFIG_PATH="$HERE/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HERE/ffmpeg_build"   --extra-cflags="-I$HERE/ffmpeg_build/include" --extra-ldflags="-L$HERE/ffmpeg_build/lib -ldl"   --bindir="$HERE/bin" --pkg-config-flags="--static"   --enable-gpl   --enable-libfdk_aac   --enable-libfreetype   --enable-libmp3lame   --enable-libopus   --enable-libvorbis   --enable-libvpx   --enable-libx264   --enable-libx265   --enable-nonfree
+  make  && make install
+  hash -r
+}
 
-tar xvf ${ffmpegfile}
-# cd ffmpeg-${ffmpegversion}
-# PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build"   --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib -ldl"   --bindir="$HOME/bin" --pkg-config-flags="--static"   --enable-gpl   --enable-libfdk_aac   --enable-libfreetype   --enable-libmp3lame   --enable-libopus   --enable-libvorbis   --enable-libvpx   --enable-libx264   --enable-libx265   --enable-nonfree
-# make  && make install
-# hash -r
+
+yasm && x264 && x265 && fdk-aac && lame && opus && ogg && vorbis && vpx && ffmpeg_build
