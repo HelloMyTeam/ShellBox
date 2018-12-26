69fff6fe04dfe0b704eb2ff56ff703d6ef901589
hello--world <hellohfy@gmail.com>
2018-12-26 16:50:04 +0800

    Update initjava.sh

--- initjava.sh
+++ initjava.sh
@@ -4,8 +4,8 @@ yum update -y
 
 cd /opt/
 
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
-
+# wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
+wget http://qiniu.xfwjfdxx.cn/jdk-8u131-linux-x64.tar.gz
 tar xzf jdk-8u131-linux-x64.tar.gz
 
 cd /opt/jdk1.8.0_131/
